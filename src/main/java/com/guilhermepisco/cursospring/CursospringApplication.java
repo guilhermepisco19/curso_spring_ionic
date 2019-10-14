763b5f70a2cf10b3c28450330875f8cb2c478f7c
Guilherme Pisco <g.pisco@campus.fct.unl.pt>
2019-10-14 19:39:26 +0100

    Creating instantiation operation

--- src/main/java/com/guilhermepisco/cursospring/CursospringApplication.java
+++ src/main/java/com/guilhermepisco/cursospring/CursospringApplication.java
@@ -1,13 +1,34 @@
 package com.guilhermepisco.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.guilhermepisco.cursospring.domain.Categoria;
+import com.guilhermepisco.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		Categoria cat1 = new Categoria(null, "Information Techonology");
+		Categoria cat2 = new Categoria(null, "Office");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
