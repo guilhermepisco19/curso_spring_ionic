c9687f91cd0310c39bf018439fac775e77cea283
Guilherme Pisco <g.pisco@campus.fct.unl.pt>
2020-06-20 13:22:55 +0100

    Saving first file on Amazon S3

--- src/main/java/com/guilhermepisco/cursospring/CursospringApplication.java
+++ src/main/java/com/guilhermepisco/cursospring/CursospringApplication.java
@@ -1,13 +1,17 @@
 package com.guilhermepisco.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guilhermepisco.cursospring.services.S3Service;
+
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner{
 	
-
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -15,6 +19,7 @@ public class CursospringApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\ASUS-PC\\Pictures\\SSL-TLS Handshaking.jpg");
 	}
 
 }
